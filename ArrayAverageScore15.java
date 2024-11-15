bd795411f3ec14a5aa882ac216a6fb238adb61f5
Akmaaal-2121 <muhamadmiftahul21@gmail.com>
2024-11-15 20:29:33 +0700

    Ex 3 After

--- ArrayAverageScore15.java
+++ ArrayAverageScore15.java
@@ -4,18 +4,37 @@ public class ArrayAverageScore15 {
         Scanner sc = new Scanner(System.in);
 
         int[] score = new int[10];
-        double total = 0;
-        double average;
+        double totalPass = 0;
+        double totalFail = 0;
+        double averageFail = 0, averagePass = 0;
+        int numStudent;
+        int pass = 0;
+        int fail = 0;
+        
+        
+        System.out.print("Enter the number of students : ");
+        numStudent = sc.nextInt();
 
-        for(int i = 0; i < score.length; i++){
-            System.out.print("Enter student score " + (i + 1) + ": ");
+        for(int i = 0; i < numStudent; i++){
+            System.out.print("Enter the final score " + i + ": ");
             score[i] = sc.nextInt();
 
         }
-        for(int i = 0; i < score.length; i++){
-            total += score[i];
+        for(int i = 0; i < numStudent; i++){
+            if(score[i] > 70){
+                totalPass += score[i];
+                pass++;
+                
+            }else{
+                totalFail += score[i];
+                fail++;
+                
+            }
         }
-        average = total / score.length;
-        System.out.println("The class average score is " + average);
+       averagePass = totalPass / pass;
+       averageFail = totalFail / fail;
+        System.out.println("The average score of students who passed is " + averagePass);
+    
+        System.out.println("The average score of students who failed is " + averageFail);
     }
 }
